4ae1f67b3ed2e548520f4e2eff5c46f53fb5e567
shashank chaudhary <97469450+Shashankch14@users.noreply.github.com>
2024-06-21 01:54:18 +0530

    d2

--- fitnessfreak/src/app/page.tsx
+++ fitnessfreak/src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import Image from "next/image";
 import styles from "./page.module.css";
 import HomeBanner2 from "@/components/HomeBanner2/HomeBanner2";
--- fitnessfreak/src/components/Navbar/Navbar.tsx
+++ fitnessfreak/src/components/Navbar/Navbar.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import logo from '@/assets/logo.png'
+import { IoIosBody } from 'react-icons/io'
+import './Navbar.css'
+import Image from 'next/image'
+import Link from 'next/link'
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+  <nav>
+      <Image src={logo} alt="fitnessLogo "/>
+      <Link href="/" >Home</Link>
+      <Link href="/about">About</Link>
+      <Link href="/profile" ><IoIosBody /></Link>
+      <button>Sign Out</button>
+
+  </nav>
+    
   )
 }
 
